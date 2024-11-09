95fe11c9f247788af1bfad0a9decb1a8b22010cc
Hiro-824 <ma8950246@gmail.com>
2024-11-09 21:28:47 +0900

    fixed main ts error

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,7 +1,9 @@
 import { Box } from "@chakra-ui/react";
 import { Main } from "./components/main";
 
-export default function Id() {
+export default function Id({params}: {params: {id: string}}) {
+
+  const { id } = params
 
   const audioUrl = "https://ggqoevksoazxxcijctks.supabase.co/storage/v1/object/public/items/1b929165-e504-4af6-9fb0-8760f362f43e/b7f6b34e-3ad3-44bd-a0b8-f76f50afafeb/audio"
   const scriptUrl = "https://ggqoevksoazxxcijctks.supabase.co/storage/v1/object/public/items/1b929165-e504-4af6-9fb0-8760f362f43e/b7f6b34e-3ad3-44bd-a0b8-f76f50afafeb/b7f6b34e-3ad3-44bd-a0b8-f76f50afafeb.json"
@@ -9,7 +11,7 @@ export default function Id() {
   return (
     <>
       <Box zIndex={-1} bg={"#F0F8FF"} position={"fixed"} top={0} bottom={0} right={0} left={0} />
-      <Main audioUrl={audioUrl} scriptUrl={scriptUrl} />
+      <Main audioUrl={audioUrl} scriptUrl={scriptUrl} id={id} />
     </>
   );
 }
