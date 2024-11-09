ec99634ac8526b107b6839fbe2fb908a557e5694
Hiro-824 <ma8950246@gmail.com>
2024-11-09 23:20:50 +0900

    trying param id

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,7 +1,10 @@
 import { Box } from "@chakra-ui/react";
 import { Main } from "./components/main";
+import { useParams } from 'next/navigation';
 
-export default async function Id() {
+export default async function Id({ params }: { params: { id: string } }) {
+
+  const id = params.id;
 
   const audioUrl = "https://ggqoevksoazxxcijctks.supabase.co/storage/v1/object/public/items/1b929165-e504-4af6-9fb0-8760f362f43e/b7f6b34e-3ad3-44bd-a0b8-f76f50afafeb/audio"
   const scriptUrl = "https://ggqoevksoazxxcijctks.supabase.co/storage/v1/object/public/items/1b929165-e504-4af6-9fb0-8760f362f43e/b7f6b34e-3ad3-44bd-a0b8-f76f50afafeb/b7f6b34e-3ad3-44bd-a0b8-f76f50afafeb.json"
@@ -9,7 +12,7 @@ export default async function Id() {
   return (
     <>
       <Box zIndex={-1} bg={"#F0F8FF"} position={"fixed"} top={0} bottom={0} right={0} left={0} />
-      <Main audioUrl={audioUrl} scriptUrl={scriptUrl} id={"test"} />
+      <Main audioUrl={audioUrl} scriptUrl={scriptUrl} id={id} />
     </>
   );
 }
