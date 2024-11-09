13d3b7bfb7d09aadea0545546f3ad7aca144774a
Hiro-824 <ma8950246@gmail.com>
2024-11-09 21:38:33 +0900

    inserted dummy id

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,11 +1,7 @@
 import { Box } from "@chakra-ui/react";
 import { Main } from "./components/main";
 
-export default async function Id({ params }: {
-  params: { id: string };
-}) {
-
-  const { id } = params
+export default async function Id() {
 
   const audioUrl = "https://ggqoevksoazxxcijctks.supabase.co/storage/v1/object/public/items/1b929165-e504-4af6-9fb0-8760f362f43e/b7f6b34e-3ad3-44bd-a0b8-f76f50afafeb/audio"
   const scriptUrl = "https://ggqoevksoazxxcijctks.supabase.co/storage/v1/object/public/items/1b929165-e504-4af6-9fb0-8760f362f43e/b7f6b34e-3ad3-44bd-a0b8-f76f50afafeb/b7f6b34e-3ad3-44bd-a0b8-f76f50afafeb.json"
@@ -13,7 +9,7 @@ export default async function Id({ params }: {
   return (
     <>
       <Box zIndex={-1} bg={"#F0F8FF"} position={"fixed"} top={0} bottom={0} right={0} left={0} />
-      <Main audioUrl={audioUrl} scriptUrl={scriptUrl} id={id} />
+      <Main audioUrl={audioUrl} scriptUrl={scriptUrl} id={"test"} />
     </>
   );
 }
