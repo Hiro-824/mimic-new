17a9a8b24369e4fb1317a83625b9ec1e18fcb056
Hiro-824 <ma8950246@gmail.com>
2024-11-14 18:59:52 +0900

    trying next link

--- src/app/home/demo-button.tsx
+++ src/app/home/demo-button.tsx
@@ -1,26 +1,31 @@
 "use client"
 
 import { Button } from "@chakra-ui/react"
-import { useRouter } from "next/navigation"
+import NextLink from "next/link";
+//import { useRouter } from "next/navigation"
 
 export const DemoButton = () => {
 
-    const router = useRouter();
+    //const router = useRouter();
+
+    const link = "/items/798b371e-4ac5-4d54-9548-a426344d937e";
 
     return (
-        <Button
-            backgroundColor="blue.100"
-            color="black"
-            //borderRadius="full"
-            borderRadius={"md"}
-            _hover={{ backgroundColor: 'blue.200' }}
-            _active={{ backgroundColor: 'blue.300' }}
-            fontWeight="bold"
-            //h="32px"
-            px={6}
-            onClick={() => router.push("/items/798b371e-4ac5-4d54-9548-a426344d937e")}
-        >
-            ログイン不要で試す
-        </Button>
+        <NextLink href={link} passHref>
+            <Button
+                backgroundColor="blue.100"
+                color="black"
+                //borderRadius="full"
+                borderRadius={"md"}
+                _hover={{ backgroundColor: 'blue.200' }}
+                _active={{ backgroundColor: 'blue.300' }}
+                fontWeight="bold"
+                //h="32px"
+                px={6}
+            //onClick={() => router.push("/items/798b371e-4ac5-4d54-9548-a426344d937e")}
+            >
+                ログイン不要で試す
+            </Button>
+        </NextLink>
     )
 }
